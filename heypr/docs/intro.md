2551afcf09e3e5331ba6d766bb459ed726812953
hyper <39167664+heypr@users.noreply.github.com>
2024-04-02 21:35:34 -0400

    Initial commit.

---
+++ heypr/docs/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Homepage
+## About me
+#### Hello! I'm hyper, 
---
+++ heypr/docusaurus.config.js
@@ -0,0 +1,39 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'hyper',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://heypr.dev',
+  baseUrl: '/',
+  projectName: 'heypr',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/',
+        },
+        blog: false,
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
